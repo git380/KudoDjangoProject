d413ac5ccb40b992479138422924160edcbfe7d3
git380 <108381952+git380@users.noreply.github.com>
2023-12-21 22:35:52 +0900

    createTable 行修正

--- createTable.py
+++ createTable.py
@@ -15,15 +15,18 @@ employee_data = [
     {"empid": "2070077", "empfname": "sho", "emplname": "kudou", "emppasswd": "11111111", "emprole": 1},
     {"empid": "333333", "empfname": "b", "emplname": "v", "emppasswd": "33333333", "emprole": 2},
     {"empid": "aaa", "empfname": "a", "emplname": "a", "emppasswd": "11111111", "emprole": 2},
+    {"empid": "1", "empfname": "1", "emplname": "1", "emppasswd": "1", "emprole": 1},
+    {"empid": "2", "empfname": "2", "emplname": "2", "emppasswd": "2", "emprole": 2},
+    {"empid": "3", "empfname": "3", "emplname": "3", "emppasswd": "3", "emprole": 3},
 ]
 
 for data in employee_data:
     models.Employee.objects.create(**data)
 
 tabyouin_data = [
-    {"tabyouinid": "1", "tabyouinmei": "nyt", "tabyouinaddres": "thrah", "tabyouintel": "090-1234-1234", "tabyouinshihonkin": 11, "kyukyu": 0},
-    {"tabyouinid": "0000", "tabyouinmei": "test", "tabyouinaddres": "広島県広島市南区比治山本町１６−３５", "tabyouintel": "111-1111-1111", "tabyouinshihonkin": 100, "kyukyu": 0},
-    {"tabyouinid": "0001", "tabyouinmei": "aaaa", "tabyouinaddres": "hiro", "tabyouintel": "111-1111-1111", "tabyouinshihonkin": 9999, "kyukyu": 1},
+    {"tabyouinid": "1", "tabyouinmei": "nyt", "tabyouinaddres": "thrah", "tabyouintel": "09012341234", "tabyouinshihonkin": 11, "kyukyu": 0},
+    {"tabyouinid": "0000", "tabyouinmei": "test", "tabyouinaddres": "広島県広島市南区比治山本町１６−３５", "tabyouintel": "11111111111", "tabyouinshihonkin": 100, "kyukyu": 0},
+    {"tabyouinid": "0001", "tabyouinmei": "aaaa", "tabyouinaddres": "hiro", "tabyouintel": "11111111111", "tabyouinshihonkin": 9999, "kyukyu": 1},
 ]
 for data in tabyouin_data:
     models.Tabyouin.objects.create(**data)
